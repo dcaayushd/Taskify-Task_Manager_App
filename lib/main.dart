411434ff95b7cf9bf687f8df13e01fcb4e9b0bca
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-06-05 07:52:17 +0545

    Firebase & Firestore Connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
+import 'package:taskify/firebase_options.dart';
 
 import '../common/show_model.dart';
 
 import '../widgets/card_todo_list_widget.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
