a5b124619c360f4b594590c54d78a15898d1b962
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-06-05 09:09:33 +0545

    New Task Model Added

---
+++ lib/model/taskify_model.dart
@@ -0,0 +1,53 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class TaskifyModel {
+  String? docID;
+  final String titleTask;
+  final String description;
+  final String category;
+  final String dateTask;
+  final String timeTask;
+
+  TaskifyModel({
+    this.docID,
+    required this.titleTask,
+    required this.description,
+    required this.category,
+    required this.dateTask,
+    required this.timeTask,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'docID': docID,
+      'titleTask': titleTask,
+      'description': description,
+      'category': category,
+      'dateTask': dateTask,
+      'timeTask': timeTask,
+    };
+  }
+
+  factory TaskifyModel.fromMap(Map<String, dynamic> map) {
+    return TaskifyModel(
+      docID: map['docID'] != null ? map['docID'] as String : null,
+      titleTask: map['titleTask'] as String,
+      description: map['description'] as String,
+      category: map['category'] as String,
+      dateTask: map['dateTask'] as String,
+      timeTask: map['timeTask'] as String,
+    );
+  }
+
+  factory TaskifyModel.fromSnapshot(
+      DocumentSnapshot<Map<String, dynamic>> doc) {
+    return TaskifyModel(
+      docID: doc.id,
+      titleTask: doc['titleTask'],
+      description: doc['description'],
+      category: doc['category'],
+      dateTask: doc['dateTask'],
+      timeTask: doc['timeTask'],
+    );
+  }
+}
